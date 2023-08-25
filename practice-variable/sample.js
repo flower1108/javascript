b3702bff97c5d0c058910c12f162daa8ce4c99b3
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-08-25 07:24:32 +0000

    javascript-lesson

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -1,1 +1,28 @@
-alert('Hello Word');
+const API_KEY = "dd56a737c6e57783bc7ec8a8f444f0bb"
+
+$(function(){
+  $('#btn').on('click', function(){
+    $.ajax({
+      url:"https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+      // 位置
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //湿度
+      $('#humidity').text(data.main.humidity);
+      //風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fall(function (data){
+      alert('通信に失敗しました。');
+    });
+  });
+});
